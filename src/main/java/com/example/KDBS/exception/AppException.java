42731f3af5a862e1e6a70c03eaebac97c82729f8
AnhMinhHoang <anhminhnamly1@gmail.com>
2025-09-11 20:01:20 +0700

    Squashed commit of the following:
    commit c6a1a8f604eae9aa5d225877b931dca87d660d8b
    Author: quanpmde <tomquan10@gmail.com>
    Date:   Thu Sep 11 19:46:27 2025 +0700
    
        fix authority p2, change filename, add post status, add more error code
    
    commit 9300aa79788faddee27b11d8a73256b399270689
    Author: quanpmde <tomquan10@gmail.com>
    Date:   Thu Sep 11 16:47:57 2025 +0700
    
        quanpmde - authorization p1, update error code, add package security
    
    commit dbe28b95195cdde75dd35999f863e9b35b307c07
    Author: quanpmde <tomquan10@gmail.com>
    Date:   Tue Sep 9 22:52:38 2025 +0700
    
        quanpmde - fix sprint 1 register and forgot password, sprint 2 suggestion

--- src/main/java/com/example/KDBS/exception/AppException.java
+++ src/main/java/com/example/KDBS/exception/AppException.java
@@ -2,11 +2,19 @@ package com.example.KDBS.exception;
 
 public class AppException extends RuntimeException{
     private ErrorCode errorCode;
+    private Object[] args; // dynamic info, ví dụ id
 
     public AppException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
+
+    public AppException(ErrorCode errorCode, Object... args) {
+        super(String.format(errorCode.getMessage(), args));
+        this.errorCode = errorCode;
+        this.args = args;
+    }
+
 
     public ErrorCode getErrorCode() {
         return errorCode;
--- src/main/java/com/example/KDBS/repository/UserRepository.java
+++ src/main/java/com/example/KDBS/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.KDBS.repository;
 
+import com.example.KDBS.enums.Status;
 import com.example.KDBS.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +17,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     boolean existsByEmail(String userEmail);
 
-    @Query(value = "SELECT * FROM User ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    @Query(value = "SELECT * FROM user ORDER BY RAND() LIMIT :limit", nativeQuery = true)
     List<User> findRandomUsers(@Param("limit") int limit);
+
+    List<User> findByStatusAndCreatedAtBefore(Status status, LocalDateTime time);
+
 }
