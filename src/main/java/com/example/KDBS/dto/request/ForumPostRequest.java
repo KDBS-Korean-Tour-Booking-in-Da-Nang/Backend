f48e8013b0a7bdf0bac72cd5a46140ca6d90ee8b
quanpmde <127415310+quanpmde@users.noreply.github.com>
2025-11-22 18:24:29 +0700

    update create post request

--- src/main/java/com/example/KDBS/dto/request/ForumPostRequest.java
+++ src/main/java/com/example/KDBS/dto/request/ForumPostRequest.java
@@ -1,6 +1,8 @@
 package com.example.KDBS.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,6 +12,8 @@ import java.util.List;
 @Data
 public class ForumPostRequest {
     @NotBlank(message = "User email is required")
+    @Email(message = "Invalid email format")
+    @Size(max = 100, message = "Email must not exceed 100 characters")
     private String userEmail;
     @NotBlank(message = "Title is required")
     private String title;
