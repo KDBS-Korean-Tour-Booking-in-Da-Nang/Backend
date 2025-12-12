28773d471a88f5b9b260247d0d36ad3ad19cfdad
AnhMinhHoang <anhminhnamly1@gmail.com>
2025-12-12 23:30:21 +0700

    Update: change AI model

--- src/main/java/com/example/KDBS/config/GeminiConfig.java
+++ src/main/java/com/example/KDBS/config/GeminiConfig.java
@@ -1,17 +1,27 @@
 package com.example.KDBS.config;
 
-import com.google.genai.Client;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class GeminiConfig {
-    @Value("${gemini.api.key}")
-    private String apiKey;
+    @Value("${spring.ai.openai.api-key}")
+    private String GROQ_API_KEY;
+
+    @Value("${spring.ai.openai.base-url}")
+    private String GROQ_API_URL;
 
     @Bean
-    public Client getClient() {
-        return Client.builder().apiKey(apiKey).build();
+    public OpenAiChatModel customGroqChatClient() {
+        OpenAiApi groqOpenAiApi = new OpenAiApi.Builder()
+                .apiKey(GROQ_API_KEY)
+                .baseUrl(GROQ_API_URL)
+                .build();
+        return OpenAiChatModel.builder()
+                .openAiApi(groqOpenAiApi)
+                .build();
     }
 }
